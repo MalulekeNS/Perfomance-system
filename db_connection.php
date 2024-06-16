0f29d7ac6caf2491af77b4b26ab4595d4408536c
Newcayen Sevezile Maluleke <157349577+MalulekeNS@users.noreply.github.com>
2024-06-17 00:56:23 +0200

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,54 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "nlsa";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Function to fetch all users from the database
+function getAllUsers() {
+    global $conn;
+    $sql = "SELECT * FROM users";
+    $result = $conn->query($sql);
+    $users = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $users[] = $row;
+        }
+    }
+    return $users;
+}
+
+// Function to add a new user to the database
+function addUser($email, $password) {
+    global $conn;
+    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Function to reset password for a user
+function resetPassword($userId, $newPassword) {
+    global $conn;
+    $sql = "UPDATE users SET password = '$newPassword' WHERE id = $userId";
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Close connection
+$conn->close();
+?>
